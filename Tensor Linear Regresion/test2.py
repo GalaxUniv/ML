c093cc3cb350f4c17bc7b61ce458565cb5d66554
Galax <GalaxCherryL@gmail.com>
2021-12-02 22:15:50 +0100

    enter

--- Tensor Linear Regresion/test2.py
+++ Tensor Linear Regresion/test2.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
 
-data = pd.read_csv("student-mat.csv", sep=";") #reads data seperator = ;
+data = pd.read_csv("Tensor Linear Regresion/student-mat.csv", sep=";") #reads data seperator = ;
 
 
 data = data[["G1","G2","G3","studytime","failures","absences"]]
-print(data.head())
+print(data.head()) #printing 5 rows
 
 predict = "G3"
 best = 0
@@ -20,7 +20,7 @@ X= np.array(data.drop([predict],1))
 y = np.array(data[predict])
 x_train , x_test ,y_train , y_test  = sklearn.model_selection.train_test_split(X,y, test_size = 0.1)
 
-''' # training model
+'''# training model
 for i in range(10000):
     x_train , x_test ,y_train , y_test  = sklearn.model_selection.train_test_split(X,y, test_size = 0.1)
 
@@ -34,12 +34,12 @@ for i in range(10000):
 
     if acc > best:
         best = acc
-        with open("studentmodel.pickle", "wb") as f:        #saves and loads 
+        with open("Tensor Linear Regresion/studentmodel.pickle", "wb") as f:        #saves and loads 
             pickle.dump(linear,f)                           #already trained 
                                                             #linear module
 '''
 
-pickle_in = open("studentmodel.pickle" ,"rb")       #
+pickle_in = open("Tensor Linear Regresion/studentmodel.pickle" ,"rb")       #
 linear = pickle.load(pickle_in) 
 
 print("Co : \n", linear.coef_) # = a in each direction
@@ -54,17 +54,4 @@ style.use("ggplot")
 pyplot.scatter(data[p],data["G3"])
 pyplot.xlabel(p)
 pyplot.ylabel("Final Grade")
-
-with open("aANDb.txt","a") as file:
-    file.write(f"{linear.coef_} : {linear.intercept_} ")
-print(linear.coef_[1] , linear.intercept_)
-a = linear.coef_[1]
-b = linear.intercept_
-
-ax = pyplot.subplot()
-t = np.arange(0,21,1)   
-s = ((a*t) + b) # a * range x + b
-line, = pyplot.plot(t, a*t + b, lw=2)
-
-
 pyplot.show()
