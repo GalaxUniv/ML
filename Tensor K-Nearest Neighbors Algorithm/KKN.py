e47b69084f8c1bc56256be93585ca9ce2b192135
Galax <GalaxCherryL@gmail.com>
2021-12-03 20:19:37 +0100

    enter

--- Tensor K-Nearest Neighbors Algorithm/KKN.py
+++ Tensor K-Nearest Neighbors Algorithm/KKN.py
@@ -24,11 +24,32 @@ door = le.fit_transform(list(data["door"]))
 persons = le.fit_transform(list(data["persons"]))
 lug_boot = le.fit_transform(list(data["lug_boot"]))
 safety = le.fit_transform(list(data["safety"]))
-class_ = le.fit_transform(list(data["class"]))
-
+class_=data["class"].map({"unacc": 0, "acc": 1,"good": 2,"vgood": 3})
 X = list(zip(buying,maint,door,persons,lug_boot,safety))
 Y = list(class_)
 
 x_train , x_test ,y_train , y_test  = sklearn.model_selection.train_test_split(X,Y, test_size = 0.15)
 
-print(x_train , y_test)
+#print(x_train , y_test)
+
+model = KNeighborsClassifier (n_neighbors=8)
+
+model.fit(x_train , y_train)
+acc = model.score(x_test, y_test)
+
+predicted = model.predict(x_test)
+names = ["unacc","acc","good","vgood"]
+
+for z in range(len(predicted)):
+  #print("Predicted:",predicted[z] ,"Data: ",x_test[z], "Actual ",y_test[z])
+  print("Predicted:",names[predicted[z]] ,"Data: ",x_test[z], "Actual ",names[y_test[z]])
+  n=model.kneighbors([x_test[z]],9 , True)
+  print(n)
+
+print(acc)
+
+
+
+
+
+
